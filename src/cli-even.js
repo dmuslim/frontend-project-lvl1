324b02c311603a9c905ce7984c6cbde8e1ba2745
dmuslim <darkhan.issin@gmail.com>
2022-05-17 12:27:11 +0600

    src/cli-even.js

--- src/cli-even.js
+++ src/cli-even.js
@@ -13,9 +13,9 @@ while (count <= 3) {
   const answer = readlineSync.question('Your answer: ');
   if ((numRounded % 2 === 0 && answer === 'yes') || (numRounded % 2 !== 0 && answer === 'no')) {
     console.log('Correct!'); count += 1;
-  } else if (numRounded % 2 !== 0 && answer === "yes") {
+  } else if (numRounded % 2 !== 0 && answer === 'yes') {
     console.log(`'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, ${name}!`); count += 10;
-  } else if (numRounded % 2 === 0 && answer === "no") {
+  } else if (numRounded % 2 === 0 && answer === 'no') {
     console.log(`'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, ${name}!`); count += 10;
   } else {
     console.log('this is wrong answer ;(.'); count += 10;
