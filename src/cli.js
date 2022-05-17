993844475affc15efedcc16e73c30e88e841f24d
dmuslim <darkhan.issin@gmail.com>
2022-05-17 12:18:38 +0600

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 
-console.log("brain-games\nWelcome to the Brain Games!");
+console.log('brain-games\nWelcome to the Brain Games!');
 export const name = readlineSync.question('May I have your name? ');
