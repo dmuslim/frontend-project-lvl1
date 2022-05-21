eb97b0c6253fb14d1013426c92a476406b950258
dmuslim <darkhan.issin@gmail.com>
2022-05-21 13:10:04 +0600

    calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import getRandomNumber from '../index.js';
+import { getRandomNumber } from '../index.js';
 
 const runGameOfCalc = () => {
   console.log('brain-calc\nWelcome to the Brain Games!');
